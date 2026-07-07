0fc02a550aba27d7bf156553f082c1d712555cad
Jeffrey Mori Ubaldini <14824@holbertonstudents.com>
2026-07-07 15:31:45 +1000

    changed syntax of m declaration

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,12 +8,13 @@ void times_table(void)
 {
 	int n1;
 	int n2;
+	int m;
 
 	for (n1 = 0 ; n1 < 10 ; n1++)
 	{
 		for (n2 = 0 ; n2 < 10 ; n2++)
 		{
-			int m = n1 * n2;
+			m = n1 * n2;
 
 			if (m > 9)
 				_putchar(m / 10 + '0');
