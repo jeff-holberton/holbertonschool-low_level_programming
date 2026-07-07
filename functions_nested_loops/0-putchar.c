6fe62fb99c03b3368fac574a055d79bee251a60c
Jeffrey Mori Ubaldini <14824@holbertonstudents.com>
2026-07-07 11:27:24 +1000

    Added folder readme, header and file 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - main function that prints the string _putchar
+ * followed by a new line
+ * Return: returns 0
+*/
+int main(void)
+{
+	char str[] = "_putchar";
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
