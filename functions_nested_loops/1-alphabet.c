15836d160ba2055648adfc963cc4cb6e981f5234
Jeffrey Mori Ubaldini <14824@holbertonstudents.com>
2026-07-07 12:00:51 +1000

    Added program 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - function that prints the alphabet
+ * in lower case, followed by a new line
+ * Return: returns 0
+*/
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 97 ; n < 123 ; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+	return;
+}
