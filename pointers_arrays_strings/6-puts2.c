8fdda6cd05ee52a2284ee32fe964ff22d7bd2c14
Jeffrey Mori Ubaldini <14824@holbertonstudents.com>
2026-07-14 13:39:32 +1000

    adjusted algorithm, introduced if condition

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,10 +7,11 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0' && str[i-1] != '\0')
+	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
-		i = i + 2;
+		if (i % 2 ==0)
+			_putchar(str[i]);
+		i++;
 	}
 	_putchar('\n');
 }
