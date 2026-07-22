8aa0e963a8f4177f86588795e2839711b5e090d3
Jeffrey Mori Ubaldini <14824@holbertonstudents.com>
2026-07-22 15:11:51 +1000

    Added first structure file

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a structure defining a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the dog's owner
+ *
+ * Description: a structure of a dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
