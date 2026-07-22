f66ec9407be29078c9f6b966dc4639b0ab7fb5b8
Jeffrey Mori Ubaldini <14824@holbertonstudents.com>
2026-07-22 16:22:19 +1000

    Created copies of strings

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,12 +12,37 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
+	char *n;
+	char *o;
+	int i = 0;
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
-		return(NULL);
-	dog->name = name;
+	{
+		free(dog);
+		return (NULL);
+	}
+	n = malloc(sizeof(name));
+	if (n == NULL)
+		return (NULL);
+	o = malloc(sizeof(owner));
+	if (o == NULL)
+		return (NULL);
+	while (name[i] != '\0')
+	{
+		n[i] = name[i];
+		i++;
+	}
+	n[i] = '\0';
+	i = 0;
+	while (owner[i] != '\0')
+	{
+		o[i] = owner[i];
+		i++;
+	}
+	o[i] = '\0';
+	dog->name = n;
 	dog->age = age;
-	dog->owner = owner;
-	return(dog);
+	dog->owner = o;
+	return (dog);
 }
