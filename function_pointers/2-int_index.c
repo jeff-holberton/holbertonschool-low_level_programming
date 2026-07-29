c5760a1ad15c2cfa49e46cebcb04dec1a7af78f7
Jeffrey Mori Ubaldini <14824@holbertonstudents.com>
2026-07-29 17:40:42 +1000

    Added Null Clause

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -15,6 +15,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
+	if (array == NULL || cmp == NULL)
+		return (-1);
 	while (i < size)
 	{
 		if (cmp(array[i]) != 0)
