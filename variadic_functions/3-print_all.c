7c09b549cbfd04843ee0f37beafbcca847a9f6c4
Jeffrey Mori Ubaldini <14824@holbertonstudents.com>
2026-08-01 13:28:08 +1000

    Fixed new NULL issue

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -14,37 +14,38 @@ void print_all(const char * const format, ...)
 	char *temp;
 
 	va_start(args, format);
-	while (format[i] != '\0' && format != NULL)
-	{
-		switch (format[i])
+	if (format != NULL)
+		while (format[i] != '\0')
 		{
-			case 'c':
-				printf("%c", (char)va_arg(args, int));
-				flag = 1;
-				break;
-			case 'i':
-				printf("%d", va_arg(args, int));
-				flag = 1;
-				break;
-			case 'f':
-				printf("%f", (float)va_arg(args, double));
-				flag = 1;
-				break;
-			case 's':
-				temp = va_arg(args, char *);
-				if (temp == NULL)
-					temp = "(nil)";
-				printf("%s", temp);
-				flag = 1;
-				break;
+			switch (format[i])
+			{
+				case 'c':
+					printf("%c", (char)va_arg(args, int));
+					flag = 1;
+					break;
+				case 'i':
+					printf("%d", va_arg(args, int));
+					flag = 1;
+					break;
+				case 'f':
+					printf("%f", (float)va_arg(args, double));
+					flag = 1;
+					break;
+				case 's':
+					temp = va_arg(args, char *);
+					if (temp == NULL)
+						temp = "(nil)";
+					printf("%s", temp);
+					flag = 1;
+					break;
+			}
+			i++;
+			while (flag == 1 && format[i] != '\0')
+			{
+				printf(", ");
+				flag = 0;
+			}
 		}
-		i++;
-		while (flag == 1 && format[i] != '\0')
-		{
-			printf(", ");
-			flag = 0;
-		}
-	}
 	va_end(args);
 	printf("\n");
 }
