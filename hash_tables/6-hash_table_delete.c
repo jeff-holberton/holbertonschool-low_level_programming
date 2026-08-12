2fa5262bdeba5a60da9ee71cb959db731a4a2103
Jeffrey Mori Ubaldini <14824@holbertonstudents.com>
2026-08-12 13:12:35 +1000

    Changed order of free

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -11,7 +11,8 @@ void hash_table_delete(hash_table_t *ht)
 	hash_node_t *current_node;
 	unsigned long int index = 0;
 
-
+	if (ht == NULL)
+		return;
 	while (index < ht->size)
 	{
 		current_node = ht->array[index];
@@ -19,8 +20,8 @@ void hash_table_delete(hash_table_t *ht)
 		{
 			current_node = current_node->next;
 			free(ht->array[index]->value);
-			free(ht->array[index]);
 			free(ht->array[index]->key);
+			free(ht->array[index]);
 			ht->array[index] = current_node;
 		}
 		index++;
